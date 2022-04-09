86b7874abd12709482692531e45a74f62ef854e6
Natália <nataliasouzaguimaraes@gmail.com>
2022-04-09 13:54:55 -0300

    customizando url

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -5,13 +5,24 @@ import { config } from '../config/Constants'
 
 export class URLController {
     public async shorten(req: Request, res: Response): Promise<void>{
-        const{ originURL } = req.body
-        const url = await URLModel.findOne({originURL})
+        const{ originURL, customURL } = req.body
+        let url = await URLModel.findOne({originURL})
+        let hash = customURL
+        const urlHash = await URLModel.findOne({hash})
+
         if (url) {
-            res.json(url)
+            res.status(409).json({error: "URL already exists", url})
             return
-        } 
-        const hash = shortId.generate()
+        }else if(urlHash){
+            url = urlHash
+            res.status(409).json({error: "Custom URL already exists", url})
+            return
+        }
+
+        if ( customURL == '' ){
+            hash = shortId.generate()
+        }
+
         const shortURL = `${config.API_URL}/${hash}`
         const newURL = await URLModel.create({hash, shortURL, originURL})
 
@@ -26,7 +37,6 @@ export class URLController {
             res.redirect(url.originURL)
             return
         }
-        // res.status(400).json({error: "URL not found"})
-        res.status(400).json(url)
+        res.status(400).json({error: "URL not found"})
     }
 }
